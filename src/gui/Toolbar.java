7b8b257b4ddf0700678a570c1a97ecd537013198
Tatjana Gemovic <gemovic.ra105.2019@uns.ac.rs>
2021-11-04 16:50:33 +0100

    dodat toolbar

--- src/gui/Toolbar.java
+++ src/gui/Toolbar.java
@@ -10,4 +10,31 @@ import javax.swing.SwingConstants;
 
 public class Toolbar extends JToolBar {
 	
+	public Toolbar() {
+
+		super(SwingConstants.HORIZONTAL);
+		JButton btnAdd = new JButton();
+		btnAdd.setToolTipText("Add");
+		btnAdd.setIcon(new ImageIcon("ikonice/add copy.png"));
+		add(btnAdd);
+
+		addSeparator();
+
+		JButton btnEdit = new JButton();
+		btnEdit.setToolTipText("Edit");
+		btnEdit.setIcon(new ImageIcon("ikonice/writing copy.png"));
+		add(btnEdit);
+
+		addSeparator();
+
+		JButton btnDelete = new JButton();
+		btnDelete.setToolTipText("Delete");
+		btnDelete.setIcon(new ImageIcon("ikonice/trash.png"));
+		add(btnDelete);
+
+		//setFloatable(true);
+		//setBackground(new Color(255, 255, 204));
+		setBackground(Color.WHITE);
+
+	}
 }
