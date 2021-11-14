d360cc7bd14f8b61a635706667740271c51f09bf
FStefanovv <filipficastefanov22@gmail.com>
2021-11-14 17:58:51 +0100

    dodao sam na svoju granu search bar u okviru toolbara

--- src/gui/MenuBar.java
+++ src/gui/MenuBar.java
@@ -9,24 +9,32 @@ import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.KeyStroke;
+import javax.swing.ImageIcon;
 
 public class MenuBar extends JMenuBar {
 	private static final long serialVersionUID = -7863806568418316202L;
 
 	public MenuBar() {
 
+		ImageIcon icn = new ImageIcon("ikonice/new.png"); 
+		
 		JMenu file = new JMenu("File");
 		file.setMnemonic(KeyEvent.VK_F);
-		JMenuItem file_new = new JMenuItem("New");
+		JMenuItem file_new = new JMenuItem("New", icn);
 		file_new.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
-		JMenuItem file_save = new JMenuItem("Save");
+		icn = new ImageIcon("ikonice/save.png");
+		JMenuItem file_save = new JMenuItem("Save", icn);
 		file_save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
 		JMenu file_open = new JMenu("Open");
 		file_open.setMnemonic(KeyEvent.VK_O);
-		JMenuItem open_studenti = new JMenuItem("Studenti");
-		JMenuItem open_predmeti = new JMenuItem("Predmeti");
-		JMenuItem open_profesori = new JMenuItem("Profesori");
-		JMenuItem open_katedre = new JMenuItem("Katedre");
+		icn = new ImageIcon("ikonice/write.png");
+		JMenuItem open_studenti = new JMenuItem("Studenti", icn);
+		icn = new ImageIcon("ikonice/bookshelf.png");
+		JMenuItem open_predmeti = new JMenuItem("Predmeti", icn);
+		icn = new ImageIcon("ikonice/presentation.png");
+		JMenuItem open_profesori = new JMenuItem("Profesori", icn);
+		icn = new ImageIcon("ikonice/customer.png");
+		JMenuItem open_katedre = new JMenuItem("Katedre", icn);
 		file_open.add(open_studenti);
 		file_open.addSeparator();
 		file_open.add(open_predmeti);
@@ -34,7 +42,8 @@ public class MenuBar extends JMenuBar {
 		file_open.add(open_profesori);
 		file_open.addSeparator();
 		file_open.add(open_katedre);
-		JMenuItem file_close = new JMenuItem("Close");
+		icn = new ImageIcon("ikonice/close.png");
+		JMenuItem file_close = new JMenuItem("Close", icn);
 		file_close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
 		file.add(file_new);
 		file.addSeparator();
@@ -46,9 +55,11 @@ public class MenuBar extends JMenuBar {
 
 		JMenu edit = new JMenu("Edit");
 		edit.setMnemonic(KeyEvent.VK_E);
-		JMenuItem edit_entity = new JMenuItem("Edit");
+		icn = new ImageIcon("ikonice/writing copy.png");
+		JMenuItem edit_entity = new JMenuItem("Edit", icn);
 		edit_entity.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
-		JMenuItem delete_entity = new JMenuItem("Delete");
+		icn = new ImageIcon("ikonice/trash.png");
+		JMenuItem delete_entity = new JMenuItem("Delete", icn);
 		delete_entity.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_DOWN_MASK));
 		edit.add(edit_entity);
 		edit.addSeparator();
@@ -56,9 +67,11 @@ public class MenuBar extends JMenuBar {
 		
 		JMenu help = new JMenu("Help");
 		help.setMnemonic(KeyEvent.VK_H);
-		JMenuItem help_help = new JMenuItem("Help");
+		icn = new ImageIcon("ikonice/help.png");
+		JMenuItem help_help = new JMenuItem("Help", icn);
 		help_help.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_DOWN_MASK));
-		JMenuItem help_about = new JMenuItem("About");
+		icn = new ImageIcon("ikonice/info.png");
+		JMenuItem help_about = new JMenuItem("About", icn);
 		help_about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
 		help.add(help_help);
 		help.addSeparator();
--- src/gui/Toolbar.java
+++ src/gui/Toolbar.java
@@ -1,13 +1,52 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
+import javax.swing.*;  
+import java.awt.event.*;  
+import javax.swing.JTextField;
 
 public class Toolbar extends JToolBar {
 	
+	public Toolbar() {
+
+		super(SwingConstants.HORIZONTAL);
+		JButton btnAdd = new JButton();
+		btnAdd.setToolTipText("Add");
+		btnAdd.setIcon(new ImageIcon("ikonice/add copy.png"));
+		add(btnAdd);
+
+		addSeparator();
+
+		JButton btnEdit = new JButton();
+		btnEdit.setToolTipText("Edit");
+		btnEdit.setIcon(new ImageIcon("ikonice/writing copy.png"));
+		add(btnEdit);
+
+		addSeparator();
+
+		JButton btnDelete = new JButton();
+		btnDelete.setToolTipText("Delete");
+		btnDelete.setIcon(new ImageIcon("ikonice/trash.png"));
+		add(btnDelete);
+
+		//setFloatable(true);
+		//setBackground(new Color(255, 255, 204));
+		setBackground(Color.WHITE);
+		
+		
+		JTextField tf1=new JTextField();
+		tf1.setEditable(true);
+		add(tf1);
+		
+		 JButton search_btn=new JButton(new ImageIcon("ikonice/search-2.png"));  
+		 add(search_btn);
+
+	}
 }
