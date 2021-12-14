9cfc2c25def96cbf6dec05b8660496652fe9005c
FStefanovv <filipficastefanov22@gmail.com>
2021-12-14 11:18:29 +0100

    Menaju se minuti

--- src/gui/StatusBar.java
+++ src/gui/StatusBar.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -12,11 +14,11 @@ import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 public class StatusBar extends JMenuBar {
-	public static JLabel nazivTaba = new JLabel();
+	public static JLabel nazivTaba = new JLabel("- Studenti");
 	public StatusBar() {
 		JPanel nazivAplikacijePanel = new JPanel();
 		nazivAplikacijePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel nazivApp = new JLabel("  Studentska sluï¿½ba");
+		JLabel nazivApp = new JLabel("  Studentska služba");
 		nazivAplikacijePanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0,Color.BLACK));
 		nazivAplikacijePanel.add(nazivApp);
 		nazivAplikacijePanel.add(nazivTaba);
@@ -26,16 +28,20 @@ public class StatusBar extends JMenuBar {
 		JPanel datumVremePanel = new JPanel();
 		datumVremePanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		
-		DateFormat formatirano = new SimpleDateFormat("HH:mm        dd.MM.yyyy.        ");;
-		Date date = new Date();
-		JLabel datumVreme = new JLabel(formatirano.format(date));
+		DateFormat formatirano = new SimpleDateFormat("HH:mm        dd.MM.yyyy.        ");
+		JLabel datumVreme = new JLabel();
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+		  public void run() {
+			  Date date = new Date(System.currentTimeMillis());
+			  datumVreme.setText(formatirano.format(date));;
+		  }
+		}, 0, 1000);
+
 		datumVremePanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0,Color.BLACK));
 		datumVremePanel.add(datumVreme);
 		
 		add(datumVremePanel);
 		
-	}
-	void setTab(String s) {
-		nazivTaba.setText(s);
 	}
 }
