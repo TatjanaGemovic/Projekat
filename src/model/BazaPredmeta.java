d06f4d93a8b13b028ceb3ad3d0ed18890f902fa6
Tatjana Gemovic <gemovic.ra105.2019@uns.ac.rs>
2022-01-09 20:32:42 +0100

    spojene grane

--- src/model/BazaPredmeta.java
+++ src/model/BazaPredmeta.java
@@ -47,13 +47,13 @@ public class BazaPredmeta{
 		Profesor p1 = new Profesor("Nebojsa", "Ralevic", datum, adresa2, "0693792839", "nralevicn@gmail.com", adresa2, "00081526", "Doktor", 15);
 		Predmet pred = new Predmet("E2 105", "Analiza1", 9, Semestar.letnji, 1, p1);
 		this.predmeti.add(pred);
-		Predmet pred1 = new Predmet("E2 103", "LPRS", 5, Semestar.zimski, 2, p1);
+		Predmet pred1 = new Predmet("E2 114", "Lprs", 5, Semestar.zimski, 2, p1);
 		this.predmeti.add(pred1);
 		
 		Profesor p2 = new Profesor("Milan", "Rapaic", datum, adresa2, "0693792839", "rapaicmilan@gmail.com", adresa2, "00081525", "Doktor", 15);
-		Predmet pred2 = new Predmet("E2 101", "SAU", 10, Semestar.letnji, 1, p2);
+		Predmet pred2 = new Predmet("E2 108", "Sau", 10, Semestar.letnji, 1, p2);
 		this.predmeti.add(pred2);
-		Predmet pred3 = new Predmet("E2 107", "MISS", 10, Semestar.zimski, 1, p2);
+		Predmet pred3 = new Predmet("E2 107", "Miss", 10, Semestar.zimski, 1, p2);
 		this.predmeti.add(pred3);
 		Predmet pred4 = new Predmet("E2 100", "SPPuRV", 5, Semestar.zimski, 1, p2);
 		this.predmeti.add(pred4);
@@ -62,8 +62,8 @@ public class BazaPredmeta{
 		Adresa adresa1 = new Adresa("Futoska", "9", "Novi Sad", "Srbija");
 		Adresa adresa21 = new Adresa("NTP", "kabinet 3", "Novi Sad", "Srbija");
 		Profesor p = new Profesor("Milan", "Rapaic", datum1, adresa1, "0693792839", "rapaicmilan@gmail.com", adresa21, "00081525", "Doktor", 15);
-		predmeti.add(new Predmet("E2 105", "Analiza1", 9, semestar, 1, null));
-		predmeti.add(new Predmet("E2 101", "Miss", 8, semestar, 1, null));
+		predmeti.add(new Predmet("E2 111", "Analiza2", 9, semestar, 1, null));
+		predmeti.add(new Predmet("E2 101", "Miss2", 8, semestar, 1, null));
 		predmeti.add(new Predmet("E2 103", "Sau", 10, semestar, 1, p));
 	}
 	
